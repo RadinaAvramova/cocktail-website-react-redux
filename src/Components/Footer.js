d95303374419d26287764a8ad8302d7a7380dbcc
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-27 21:51:06 +0300

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="mt-4 bg-dark text-white">
       <div className="container p-4 text-center">
-        <h2>Design And Developed By : TECHINFOYT </h2>
-        <h6>All Right Reserved by &copy; Techinfoty </h6>
+        <h2>Design And Developed By :  </h2>
+        <h6>All Right Reserved by &copy;  </h6>
       </div>
     </div>
   );
